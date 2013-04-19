1d56c4fdefd0013ef972a61b576cb1424787b498
Erik <erikskoglund88@gmail.com>
2013-04-19 14:29:56 +0200

    fixed relative run commadn

---
+++ config/config.rb
@@ -0,0 +1,6 @@
+module LD26
+	WIDTH = 1280
+	HEIGHT = 720
+	FULLSCREEN = false
+	TITLE = "Ludum Dare 26 (Skoggy)"
+end
--- lib/main.rb
+++ lib/main.rb
@@ -1,9 +1,25 @@
 require 'gosu'
+require_relative '../config/config'
 
 module LD26
 	class GameWindow < Gosu::Window
 		def initialize
-			super 800, 600, false
+			super WIDTH, HEIGHT, FULLSCREEN
+			self.caption = TITLE
+		end
+
+		def button_down(id)
+			case id
+			when Gosu::KbEscape
+				self.close
+			end
+		end
+
+		def update
+			dt = 16.0
+		end
+
+		def draw
 		end
 	end
 end
