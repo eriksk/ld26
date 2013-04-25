a46eeb5a7f09ba4ba7fd9f08709450d48f6c310d
Erik <erikskoglund88@gmail.com>
2013-04-25 11:00:18 +0200

    herlpers

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -7,6 +7,32 @@ module LD26
 		clr.alpha = a
 		clr
 	end
+
+	# linear interpolation
+	def self.lerp x, y, w
+		x + (y - x) * w
+	end
+	
+	# cubic interpolation using a hermite spline, aka smooth step
+	def self.qlerp(min, max, weight)
+		hermite(min, 0.0, max, 0.0, weight)
+	end
+	
+	# value1, tangent1, value2, tangent2
+	def self.hermite(v1, t1, v2, t2, weight)
+		sCubed = weight * weight * weight
+		sSquared = weight * weight
+		result = 0.0
+		if weight == 0.0
+			result = v1
+		elsif weight == 1.0
+			result = v2
+		else
+		result = (2 * v1 - 2 * v2 + t2 + t1) * sCubed + (3 * v2 - 3 * v1 - 2 * t1 - t2) * sSquared + t1 * weight + v1
+	end
+result
+end
+	
 end
 
 class Numeric
--- lib/scenes/splash_scene.rb
+++ lib/scenes/splash_scene.rb
@@ -4,14 +4,18 @@ module LD26
 		def initialize game
 			super game
 			@logo = Entity.new(game.window.load_image("ld_logo"))
-										.set_position(WIDTH / 2.0, HEIGHT / 2.0)
+										.set_position(WIDTH / 2.0, -1024)
 										.set_scale(0.5)
 			@time = 0.0
-			@duration = 2000
+			@duration = 3000
 		end
 
 		def update dt
 			@time += dt
+			if @time < @duration / 2.0
+				@logo.position.y = LD26.qlerp(-1024, HEIGHT / 2.0, @time / (@duration / 2.0))
+			end
+
 			if @time > @duration
 				@game.pop_scene
 				@game.push_scene GameScene.new @game
