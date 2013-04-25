daa99e264164649e3af6c017b2b37a0711de7dff
Erik <erikskoglund88@gmail.com>
2013-04-25 12:55:37 +0200

    added camera

--- config/config.rb
+++ config/config.rb
@@ -30,6 +30,7 @@ def req_dir d
 end
 
 req "vec2"
+req "camera"
 req "helpers"
 req "entity"
 req "scene"
---
+++ lib/camera.rb
@@ -0,0 +1,26 @@
+module LD26
+	class Camera
+		def initialize window
+			@window = window
+			@position = Vec2.new
+			@target = Vec2.new
+			@origin = Vec2.new WIDTH / 2.0, HEIGHT / 2.0
+		end
+
+
+		def move x, y
+			@target.x, @target.y = x, y
+		end
+
+		def update dt
+			@position.x = LD26.lerp(@position.x, @target.x, @speed)
+			@position.y = LD26.lerp(@position.y, @target.y, @speed)
+		end
+
+		def translate &render_code
+			@window.translate(-@position.x + @origin.x, -@position.y - @origin.y) do
+				render_code.call()
+			end
+		end
+	end
+end
